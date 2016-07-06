51e79ff7b4498d5d36fa7019e5477c49d7f44829
Jerome Hwang <jhwang09@gmail.com>
2016-07-06 17:55:52 -0400

    changes for deliveary

--- twilio/messages.go
+++ twilio/messages.go
@@ -65,23 +65,25 @@ type MessageIterator struct {
 	client   *Client
 }
 
-func (m *MessageService) Create(data url.Values) (Message, error) {
-	msg := new(Message)
+func (m *MessageService) Create(data url.Values) (MessageDetails, error) {
+	msg := new(MessageDetails)
 	resp, err := m.client.MakeRequest("POST", pathPart, data, msg)
 	if err != nil {
 		return *msg, err
 	}
-	if resp.StatusCode < 200 && resp.StatusCode > 299 {
-		return *msg, fmt.Errorf("request not successful, status=%s, statusCode=%d", resp.Status, resp.StatusCode)
+	if resp.StatusCode < 200 || resp.StatusCode > 299 {
+		return *msg, fmt.Errorf("send SMS not successful, status=%s", resp.Status)
 	}
+
 	return *msg, nil
 }
 
-func (m *MessageService) SendMessage(from string, to string, body string, mediaUrls []url.URL) (Message, error) {
+func (m *MessageService) SendMessage(from string, to string, body string, mediaUrls []url.URL) (MessageDetails, error) {
 	v := url.Values{}
 	v.Set("Body", body)
 	v.Set("From", from)
 	v.Set("To", to)
+	v.Set("StatusCallback", "http://87b38726.ngrok.io/api/webhooks/twilio/status")
 	if mediaUrls != nil {
 		for _, mediaUrl := range mediaUrls {
 			v.Add("MediaUrl", mediaUrl.String())
